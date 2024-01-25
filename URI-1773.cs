a66f5e40992a322ba06c0090df1ddd548aced1ee
nikola7Kmotobe <140967008+nikola7Kmotobe@users.noreply.github.com>
2024-01-24 22:39:56 -0300

    Add files via upload

---
+++ URI-1773.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class URI_1173
+{
+    public static void Main(string[] args)
+    {
+        int V = int.Parse(Console.ReadLine()!);
+        int D = 0;
+        int[] N = new int[10];
+
+        while (V > 50)
+        {
+            V = int.Parse(Console.ReadLine()!);
+        }
+
+        for (int i = 0; i < N.Length; i++)
+        {
+            D = V;
+            N[i] = D;
+            V = V * 2;
+            // Console.WriteLine($"N[{i}] = {N[i]}");
+        }
+
+        for (int i = 0; i < N.Length; i++)
+        {
+            Console.WriteLine($"N[{i}] = {N[i]}");
+        }
+
+    }
+}
