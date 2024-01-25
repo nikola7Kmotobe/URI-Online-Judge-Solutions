1c952191921626dfd93b85468fcfe87e17bf1d2f
nikola7Kmotobe <140967008+nikola7Kmotobe@users.noreply.github.com>
2024-01-24 22:46:36 -0300

    Add files via upload

---
+++ URI-2218.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int T = int.Parse(Console.ReadLine());
+        int Soma = 0;
+        int[] N = new int[T];
+
+        for (int i = 0; i < N.Length; i++)
+        {
+            int qtdLinhas = int.Parse(Console.ReadLine());
+            Soma = ((qtdLinhas * (qtdLinhas + 1) / 2)) + 1;
+            N[i] = Soma;
+        }
+
+        for (int i = 0; i < N.Length; i++)
+        {
+            Console.WriteLine($"{N[i]}");
+        }
+
+    }
+
+}
