55c427cbf5313a271e1c5de470b3b4303f6c75ec
nikola7Kmotobe <140967008+nikola7Kmotobe@users.noreply.github.com>
2024-01-31 21:07:03 -0300

    Create URI-3303.cs

---
+++ URI-3303.cs
@@ -0,0 +1,17 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+      
+        string palavra = Console.ReadLine();
+        if (palavra.Length >= 10)
+        {
+            Console.WriteLine("palavrao");
+        }
+        else
+        {
+            Console.WriteLine("palavrinha");
+        }
+    }
+}
